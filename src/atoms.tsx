51478dae18982d2b062e254cfb125f6919ef4e51
Lee-Sunho <sunho9889@soongsil.ac.kr>
2023-05-30 23:25:33 +0900

    feat: local storage 저장 기능 구현

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -9,6 +9,19 @@ interface IToDoState {
   [key: string]: IToDo[];
 }
 
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue !== null && savedValue !== undefined) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: IToDoState) => {
+      localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
+
 export const toDoState = atom<IToDoState>({
   key: "todo",
   default: {
@@ -16,4 +29,5 @@ export const toDoState = atom<IToDoState>({
     Doing: [],
     Done: [],
   },
+  effects: [localStorageEffect("toDoState")],
 });
